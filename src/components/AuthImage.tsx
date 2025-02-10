3cdaacd0fc82bff29e61dcb45b570ce061d10522
oluoko <brianotieno586@gmail.com>
2025-02-10 16:45:30 +0300

    Cloud9: BrandLogo Styling

--- src/components/AuthImage.tsx
+++ src/components/AuthImage.tsx
@@ -1,10 +1,4 @@
-"use client";
-
-import logoWhiteText from "../../public/logo white text.svg";
-import logoBlackText from "../../public/logo black text.svg";
-import Image from "next/image";
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
+import { BrandLogo } from "./BrandLogo";
 
 interface AuthImageProps {
   containerStyling: string;
@@ -21,28 +15,9 @@ export function AuthImage({
   text,
   textStyling,
 }: AuthImageProps) {
-  const { theme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-
-  // useEffect only runs on the client, so now we can safely show the UI
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  // If the component is not yet mounted, return null or a placeholder
-  if (!mounted) {
-    return null;
-  }
-
   return (
     <div className={"w-[70% ] flex flex-col items-center justify-center"}>
-      <div className="w-[50vw] md:w-[30vw]">
-        {theme === "dark" ? (
-          <Image src={logoWhiteText} alt="Logo in white text" />
-        ) : (
-          <Image src={logoBlackText} alt="Logo in black text" />
-        )}
-      </div>
+      <BrandLogo styling="w-[50vw] md:w-[30vw]" />
 
       <div
         className={`${containerStyling} flex flex-col items-center justify-center `}
--- src/components/BrandLogo.tsx
+++ src/components/BrandLogo.tsx
@@ -1,9 +1,44 @@
-export function BrandLogo() {
+"use client";
+
+import logoWhiteText from "../../public/logo white text.svg";
+import logoBlackText from "../../public/logo black text.svg";
+import Image from "next/image";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+interface BrandLogoProps {
+  styling: string;
+}
+
+export function BrandLogo({ styling }: BrandLogoProps) {
+  const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // If the component is not yet mounted, return null or a placeholder
+  if (!mounted) {
+    return null;
+  }
+
   return (
-    <span className="flex items-center justify-center bg-transparent">
-      <span className="text-[35px] font-bold">
-        Cloud<span className="text-[50px] text-primary">9</span>
-      </span>
-    </span>
+    <div className={`${styling}`}>
+      {theme === "dark" ? (
+        <Image
+          src={logoWhiteText}
+          className="size-full"
+          alt="Logo in white text"
+        />
+      ) : (
+        <Image
+          src={logoBlackText}
+          className="size-full"
+          alt="Logo in black text"
+        />
+      )}
+    </div>
   );
 }
