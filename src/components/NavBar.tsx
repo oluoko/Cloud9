76f521037bda9da98e8b66376ef3daa471457b77
oluoko <brianotieno586@gmail.com>
2025-02-10 16:56:15 +0300

    Cloud9: Updated NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,7 +8,7 @@ export function NavBar() {
     <header className="flex items-center justify-center py-1 fixed shadow-xl top-0 w-screen z-99 bg-background/90">
       <nav className="flex items-center justify-between  gap-10 container font-semibold w-11/12 ">
         <Link href="/">
-          <BrandLogo styling="h-[45px] md:h-[70px] w-[90px] md:w-[140px]" />
+          <BrandLogo styling="h-[45px] md:h-[60px] w-[90px] md:w-[120px]" />
         </Link>
         <div className="flex items-center justify-between  gap-5">
           <SignedIn>
