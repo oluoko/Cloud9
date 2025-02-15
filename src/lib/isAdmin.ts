ae21a6b1e0d9fb657946b80d1c12280e2708e928
oluoko <brianotieno586@gmail.com>
2025-02-15 13:27:12 +0300

    Cloud9: Updated isAdmin.ts file

--- src/lib/isAdmin.ts
+++ src/lib/isAdmin.ts
@@ -1,20 +1,12 @@
-// import { UserResource } from "@clerk/types";
+import type { UserResource } from "@clerk/types";
 
-// type Props = {
-//   user: UserResource | null | undefined;
-// };
-import type { User } from "@clerk/nextjs/server";
-export function isAdmin(user: User | null | undefined) {
-  if (
+export function isAdmin(user: UserResource | null | undefined) {
+  return (
     user?.publicMetadata?.role === "admin" ||
     user?.publicMetadata?.role === "main_admin"
-  ) {
-    return true;
-  }
+  );
 }
 
-export function isMainAdmin(user: User | null | undefined) {
-  if (user?.publicMetadata.role === "main_admin") {
-    return true;
-  }
+export function isMainAdmin(user: UserResource | null | undefined) {
+  return user?.publicMetadata?.role === "main_admin";
 }
