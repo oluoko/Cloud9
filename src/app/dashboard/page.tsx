fa39b4addf80efbd29cebed49bf8dd1e79f71138
oluoko <brianotieno586@gmail.com>
2025-02-16 17:48:47 +0300

    Cloud9: Updated Styling

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,27 +1,10 @@
-"use client";
-
 import Footer from "@/components/Footer";
 import { Hero } from "@/components/Hero";
-import Loader from "@/components/Loader";
-import { Button } from "@/components/ui/button";
-import { useState } from "react";
 
 export default function Home() {
-  const [loading, setLoading] = useState(false);
-  const showLoader = () => {
-    setLoading(true);
-    // setTimeout(() => {
-    //   setLoading(false);
-    // }, 20000);
-  };
-
-  if (loading) {
-    return <Loader />;
-  }
   return (
     <div className="">
       <Hero />
-      <Button onClick={showLoader}>Show loader for Styling</Button>
       <Footer />
     </div>
   );
