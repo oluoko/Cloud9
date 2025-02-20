15a834fcbc492f8cd354f6c5354d3b73bb83bbee
oluoko <brianotieno586@gmail.com>
2025-02-20 13:58:55 +0300

    Cloud9: Updated Layout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,19 +2,10 @@ import Link from "next/link";
 import { BrandLogo } from "./BrandLogo";
 import { ModeToggle } from "./dark-mode-toggel";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
-import { isAdmin } from "@/lib/isAdmin";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
-import { Menu } from "lucide-react";
 import CustomUserButton from "./CustomUserButton";
 
 export function NavBar() {
-  // const { user } = useUser();
   return (
     <header className="flex items-center justify-center py-1 fixed shadow-xl top-0 w-screen z-99 bg-background/90">
       <nav className="flex items-center justify-between  gap-10 container font-semibold w-11/12 ">
@@ -23,7 +14,6 @@ export function NavBar() {
         </Link>
         <div className="flex items-center justify-between  gap-5">
           <SignedIn>
-            {" "}
             <Link
               href="/dashboard"
               className="bg-secondary/50 border border-secondary rounded-[10px] px-2 py-1"
@@ -39,7 +29,9 @@ export function NavBar() {
               </span>
             </Link>
             <Link href="/sign-in">
-              <Button variant="outline" className="rounded-[10px] px-2 py-1">Sign In</Button>
+              <Button variant="outline" className="rounded-[10px] px-2 py-1">
+                Sign In
+              </Button>
             </Link>
           </SignedOut>
           <div className="">
