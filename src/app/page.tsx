99ed0ac8c55aa8dcc097f9ebf91a6e5650607369
oluoko <brianotieno586@gmail.com>
2025-02-21 09:15:43 +0300

    Cloud9: Updated app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import AdminButton from "@/components/AdminButton";
 import Footer from "@/components/Footer";
 import { Hero } from "@/components/Hero";
 import { NavBar } from "@/components/NavBar";
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <>
       <NavBar />
+      <AdminButton />
       <div className="overflow-hidden">
         <Hero />
         <Footer />
