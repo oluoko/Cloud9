00065f42ee3c7b5ccdf78d8bdccece915febc8af
oluoko <brianotieno586@gmail.com>
2025-03-11 12:14:44 +0300

    Cloud9: Twitio update

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,17 @@
+import { string } from "zod";
+
 export function getInitials(firstName: string, secondName: string): string {
   return `${firstName[0]}.${secondName[0]}`;
 }
 
 export function getImageKey(src: string) {
   return src.substring(src.lastIndexOf("/") + 1);
+}
+
+export function generateRandomSixDigitNumber(): string {
+  const randomNum = Math.floor(100000 + Math.random());
+  const randomSixDigitString = randomNum.toString().padStart(6, "0");
+  return randomSixDigitString;
 }
 
 // to delete and image maker a request to this route:
