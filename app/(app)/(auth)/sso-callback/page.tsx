3b1ec4839a9a04f1e2f5823300cc0c26e0777a43
oluoko <brianotieno586@gmail.com>
2025-06-21 15:44:32 +0300

    Cloud9: Updated sso-callback/page.tsx

--- app/(app)/(auth)/sso-callback/page.tsx
+++ app/(app)/(auth)/sso-callback/page.tsx
@@ -1,4 +1,3 @@
-import AuthLayout from "@/components/auth-layout";
 import { AuthenticateWithRedirectCallback } from "@clerk/nextjs";
 
 export default function SSOCallback() {
@@ -6,11 +5,9 @@ export default function SSOCallback() {
   // or rendering the prebuilt <AuthenticateWithRedirectCallback/> component.
   // This is the final step in the custom OAuth flow.
   return (
-    <AuthLayout mode="login">
-      <AuthenticateWithRedirectCallback
-        signUpForceRedirectUrl="/saving-info"
-        signInForceRedirectUrl="/saving-info"
-      />
-    </AuthLayout>
+    <AuthenticateWithRedirectCallback
+      signUpForceRedirectUrl="/saving-info"
+      signInForceRedirectUrl="/saving-info"
+    />
   );
 }
