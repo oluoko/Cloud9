0cdce1abb3cc3fb3a2307578cb11ffa979173321
oluoko <brianotieno586@gmail.com>
2025-06-24 18:19:20 +0300

    Cloud9: Updated profile/page.tsx

--- app/(app)/(home)/profile/page.tsx
+++ app/(app)/(home)/profile/page.tsx
@@ -1,8 +1,13 @@
+import Footer from "@/components/footer";
+
 export default function ProfilePage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold mb-4">Profile Page</h1>
-      <p className="text-gray-600">This is the profile page.</p>
+    <div className="overflow-hidden">
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-2xl font-bold mb-4">Profile Page</h1>
+        <p className="text-gray-600">This is the profile page.</p>
+      </div>
+      <Footer />
     </div>
   );
 }
