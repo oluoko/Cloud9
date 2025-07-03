f8c508b186dc45a32024d480180edfc2dc8bff8f
oluoko <brianotieno586@gmail.com>
2025-07-03 12:27:43 +0300

    Cloud9: Updated bookings/page.tsx

--- app/(app)/(home)/(user-dashboard)/bookings/page.tsx
+++ app/(app)/(home)/(user-dashboard)/bookings/page.tsx
@@ -28,8 +28,7 @@ import {
 } from "@/components/ui/table";
 import prisma from "@/utils/db";
 import Image from "next/image";
-import { capitalize, getStatusBadgeVariant } from "@/lib/utils";
-import Footer from "@/components/footer";
+import { capitalize } from "@/lib/utils";
 
 async function getData(id: string) {
   const data = await prisma.booking.findMany({
@@ -143,6 +142,31 @@ export default async function Bookings() {
               <p className="text-xl font-bold">No Booked Flight Yet.</p>
             </div>
           )}
+          <CardFooter>
+            <div className="mt-8 flex flex-col md:flex-row items-center justify-between gap-4">
+              <p className="text-sm text-muted-foreground text-center md:text-left">
+                Thank you for booking with us. If you have any questions,{" "}
+                <Link
+                  href="/#contact-us"
+                  className="underline hover:text-primary"
+                >
+                  please contact our support team
+                </Link>
+              </p>
+              <div className="grid md:flex gap-3 w-full md:w-auto">
+                <Button
+                  asChild
+                  variant="outline"
+                  className="flex-1 md:flex-auto"
+                >
+                  <Link href="/flights">Find More Flights</Link>
+                </Button>
+                <Button asChild className="flex-1 md:flex-auto">
+                  <Link href="/">Go Back Home</Link>
+                </Button>
+              </div>
+            </div>
+          </CardFooter>
         </CardContent>
       </Card>
     </div>
