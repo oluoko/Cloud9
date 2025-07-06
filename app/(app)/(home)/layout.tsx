d250d299dae6fcdc2311da9eaaf7c3db68ece525
oluoko <brianotieno586@gmail.com>
2025-07-06 15:59:28 +0300

    Cloud9: Updated HomePageLayout

--- app/(app)/(home)/layout.tsx
+++ app/(app)/(home)/layout.tsx
@@ -2,13 +2,18 @@
 
 import { NavBar } from "@/components/nav-bar";
 import { ReactNode } from "react";
-import { BookIcon } from "lucide-react";
+import { BookIcon, PlaneIcon } from "lucide-react";
 import NavLinks from "@/components/nav-links";
 import AdminButton from "@/components/admin-button";
 import Footer from "@/components/footer";
 
 export default function HomePageLayout({ children }: { children: ReactNode }) {
   const HomePageNavLinks = [
+    {
+      label: "Flights",
+      Icon: PlaneIcon,
+      href: "/flights",
+    },
     {
       label: "Bookings",
       Icon: BookIcon,
