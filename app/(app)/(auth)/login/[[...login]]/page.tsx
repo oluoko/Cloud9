94cf53224ced459bdb3935ebce8df4f9af57c440
oluoko <brianotieno586@gmail.com>
2025-07-09 19:19:45 +0300

    Cloud9: Updated Auth pages UI

--- app/(app)/(auth)/login/[[...login]]/page.tsx
+++ app/(app)/(auth)/login/[[...login]]/page.tsx
@@ -91,97 +91,95 @@ export default function SignInForm() {
 
   return (
     <AuthLayout mode="login">
-      <div className="flex flex-col items-center justify-center">
-        <Card className="w-[90vw] md:w-full max-w-md">
-          <CardHeader>
-            <CardTitle className="font-bold text-center text-2xl">
-              Welcome back to
-              <span className="font-black text-3xl">
-                {" "}
-                Cloud
-                <span className="text-primary text-4xl">9</span>
-              </span>
-              . Sign in to continue.
-            </CardTitle>
-          </CardHeader>
-          <CardContent>
-            {/* <div className="space-y-6">
+      <Card className="w-[90vw] md:w-full max-w-md">
+        <CardHeader>
+          <CardTitle className="font-bold text-center text-2xl">
+            Welcome back to
+            <span className="font-black text-3xl">
+              {" "}
+              Cloud
+              <span className="text-primary text-4xl">9</span>
+            </span>
+            . Sign in to continue.
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          {/* <div className="space-y-6">
             <SocialAuthButtons mode="sign-in" />
           </div> */}
-            <form onSubmit={handleSubmit} className="space-y-4">
-              <div className="space-y-2">
-                <Label htmlFor="email">Email address</Label>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Email address</Label>
+              <Input
+                onChange={(e) => setEmail(e.target.value)}
+                id="email"
+                name="email"
+                type="email"
+                value={email}
+                disabled={isLoading}
+                required
+              />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="password">Enter password</Label>
+              <div className="relative">
                 <Input
-                  onChange={(e) => setEmail(e.target.value)}
-                  id="email"
-                  name="email"
-                  type="email"
-                  value={email}
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   disabled={isLoading}
                   required
                 />
+                <button
+                  type="button"
+                  className="absolute right-4 top-1/2 -translate-y-1/2"
+                  onClick={() => setShowPassword(!showPassword)}
+                  disabled={isLoading}
+                >
+                  {showPassword ? (
+                    <EyeOff className="size-5 text-foreground" />
+                  ) : (
+                    <Eye className="size-5 text-foreground" />
+                  )}
+                </button>
               </div>
-              <div className="space-y-2">
-                <Label htmlFor="password">Enter password</Label>
-                <div className="relative">
-                  <Input
-                    type={showPassword ? "text" : "password"}
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    disabled={isLoading}
-                    required
-                  />
-                  <button
-                    type="button"
-                    className="absolute right-4 top-1/2 -translate-y-1/2"
-                    onClick={() => setShowPassword(!showPassword)}
-                    disabled={isLoading}
-                  >
-                    {showPassword ? (
-                      <EyeOff className="size-5 text-foreground" />
-                    ) : (
-                      <Eye className="size-5 text-foreground" />
-                    )}
-                  </button>
-                </div>
-              </div>
-              {errors && (
-                <Alert>
-                  {errors.map((el, index) => (
-                    <AlertDescription key={index}>
-                      {el.longMessage}
-                    </AlertDescription>
-                  ))}
-                </Alert>
-              )}
+            </div>
+            {errors && (
+              <Alert>
+                {errors.map((el, index) => (
+                  <AlertDescription key={index}>
+                    {el.longMessage}
+                  </AlertDescription>
+                ))}
+              </Alert>
+            )}
 
-              <SubmitButton
-                isPending={isLoading}
-                useFormStatus={false}
-                type="submit"
-                className="w-full"
-                text="Log In"
-                loadingText="Logging In"
-              />
+            <SubmitButton
+              isPending={isLoading}
+              useFormStatus={false}
+              type="submit"
+              className="w-full"
+              text="Log In"
+              loadingText="Logging In"
+            />
 
-              <Separator text="Or" />
-              <OauthSignIn />
-            </form>
-          </CardContent>
-          <CardFooter className="justify-center text-sm">
-            <p className="text-muted-foreground">
-              Don&apos;t have an account?{" "}
-              <Link
-                href="/register"
-                className="font-medium text-primary hover:underline"
-              >
-                Register a new one.
-              </Link>
-            </p>
-          </CardFooter>
-        </Card>
-      </div>
+            <Separator text="Or" />
+            <OauthSignIn />
+          </form>
+        </CardContent>
+        <CardFooter className="justify-center text-sm">
+          <p className="text-muted-foreground">
+            Don&apos;t have an account?{" "}
+            <Link
+              href="/register"
+              className="font-medium text-primary hover:underline"
+            >
+              Register a new one.
+            </Link>
+          </p>
+        </CardFooter>
+      </Card>
     </AuthLayout>
   );
 }
--- app/(app)/(auth)/register/[[...register]]/page.tsx
+++ app/(app)/(auth)/register/[[...register]]/page.tsx
@@ -135,142 +135,98 @@ export default function SignUpPage() {
 
   return (
     <AuthLayout mode="register">
-      <div className="flex flex-col items-center justify-center">
-        <Card className="w-[90vw] md:w-full max-w-md my-4">
-          <CardHeader>
-            {!pendingVerification ? (
-              <CardTitle className="font-bold text-center text-2xl">
-                Welcome to
-                <span className="font-black text-3xl">
-                  {" "}
-                  Cloud
-                  <span className="text-primary text-4xl">9</span>
-                </span>
-                . Sign up to get started.
-              </CardTitle>
-            ) : (
-              <CardTitle className="font-bold text-center text-2xl">
-                Verify your email
-              </CardTitle>
-            )}
-          </CardHeader>
-          <CardContent>
-            {!pendingVerification ? (
-              <>
-                <form onSubmit={handleSubmit} className="space-y-4">
-                  <div className="space-y-2">
-                    <Label htmlFor="email">Email Address</Label>
+      <Card className="w-[90vw] md:w-full max-w-md">
+        <CardHeader>
+          {!pendingVerification ? (
+            <CardTitle className="font-bold text-center text-2xl">
+              Welcome to
+              <span className="font-black text-3xl">
+                {" "}
+                Cloud
+                <span className="text-primary text-4xl">9</span>
+              </span>
+              . Sign up to get started.
+            </CardTitle>
+          ) : (
+            <CardTitle className="font-bold text-center text-2xl">
+              Verify your email
+            </CardTitle>
+          )}
+        </CardHeader>
+        <CardContent>
+          {!pendingVerification ? (
+            <>
+              <form onSubmit={handleSubmit} className="space-y-4">
+                <div className="space-y-2">
+                  <Label htmlFor="email">Email Address</Label>
+                  <Input
+                    type="email"
+                    id="email"
+                    value={emailAddress}
+                    onChange={(e) => setEmailAddress(e.target.value)}
+                    disabled={isLoading}
+                    required
+                  />
+                </div>
+
+                <div className="space-y-2">
+                  <Label htmlFor="password">Password</Label>
+                  <div className="relative">
                     <Input
-                      type="email"
-                      id="email"
-                      value={emailAddress}
-                      onChange={(e) => setEmailAddress(e.target.value)}
+                      type={showPassword ? "text" : "password"}
+                      id="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
                       disabled={isLoading}
                       required
                     />
+                    <button
+                      type="button"
+                      className="absolute right-4 top-1/2 -translate-y-1/2"
+                      onClick={() => setShowPassword(!showPassword)}
+                      disabled={isLoading}
+                    >
+                      {showPassword ? (
+                        <EyeOff className="size-5 text-foreground" />
+                      ) : (
+                        <Eye className="size-5 text-foreground" />
+                      )}
+                    </button>
                   </div>
-
-                  <div className="space-y-2">
-                    <Label htmlFor="password">Password</Label>
-                    <div className="relative">
-                      <Input
-                        type={showPassword ? "text" : "password"}
-                        id="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        disabled={isLoading}
-                        required
-                      />
-                      <button
-                        type="button"
-                        className="absolute right-4 top-1/2 -translate-y-1/2"
-                        onClick={() => setShowPassword(!showPassword)}
-                        disabled={isLoading}
-                      >
-                        {showPassword ? (
-                          <EyeOff className="size-5 text-foreground" />
-                        ) : (
-                          <Eye className="size-5 text-foreground" />
-                        )}
-                      </button>
-                    </div>
-                  </div>
-
-                  <div className="space-y-2">
-                    <Label htmlFor="confirm-password">Confirm Password</Label>
-                    <div className="relative">
-                      <Input
-                        type={showPassword ? "text" : "password"}
-                        id="confirm-password"
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                        disabled={isLoading}
-                        required
-                      />
-                      <button
-                        type="button"
-                        className="absolute right-4 top-1/2 -translate-y-1/2"
-                        onClick={() => setShowPassword(!showPassword)}
-                        disabled={isLoading}
-                      >
-                        {showPassword ? (
-                          <EyeOff className="size-5 text-foreground" />
-                        ) : (
-                          <Eye className="size-5 text-foreground" />
-                        )}
-                      </button>
-                    </div>
-                  </div>
-
-                  {passwordMismatch && (
-                    <Alert>
-                      <AlertDescription>
-                        Passwords do not match.
-                      </AlertDescription>
-                    </Alert>
-                  )}
-
-                  {errors && (
-                    <Alert>
-                      {errors.map((el, index) => (
-                        <AlertDescription key={index}>
-                          {el.longMessage}
-                        </AlertDescription>
-                      ))}
-                    </Alert>
-                  )}
-
-                  <div id="clerk-captcha"></div>
-
-                  <SubmitButton
-                    isPending={isLoading}
-                    className="w-full"
-                    type="submit"
-                    text="Sign Up"
-                    loadingText="Signing you up"
-                  />
-                  <Separator text="Or" />
-                  <OauthSignIn />
-                </form>
-              </>
-            ) : (
-              <form onSubmit={handleVerify} className="space-y-4">
-                <div className="space-y-2 flex flex-col items-center justify-center">
-                  <Label htmlFor="code">Verification Code</Label>
-                  <InputOTP
-                    maxLength={codeLength}
-                    pattern={REGEXP_ONLY_DIGITS_AND_CHARS}
-                    value={code}
-                    onChange={(value) => setCode(value)}
-                    disabled={isLoading}
-                  >
-                    <InputOTPGroup>
-                      {[...Array(codeLength)].map((_, i) => (
-                        <InputOTPSlot key={i} index={i} />
-                      ))}
-                    </InputOTPGroup>
-                  </InputOTP>
                 </div>
+
+                <div className="space-y-2">
+                  <Label htmlFor="confirm-password">Confirm Password</Label>
+                  <div className="relative">
+                    <Input
+                      type={showPassword ? "text" : "password"}
+                      id="confirm-password"
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                      disabled={isLoading}
+                      required
+                    />
+                    <button
+                      type="button"
+                      className="absolute right-4 top-1/2 -translate-y-1/2"
+                      onClick={() => setShowPassword(!showPassword)}
+                      disabled={isLoading}
+                    >
+                      {showPassword ? (
+                        <EyeOff className="size-5 text-foreground" />
+                      ) : (
+                        <Eye className="size-5 text-foreground" />
+                      )}
+                    </button>
+                  </div>
+                </div>
+
+                {passwordMismatch && (
+                  <Alert>
+                    <AlertDescription>Passwords do not match.</AlertDescription>
+                  </Alert>
+                )}
+
                 {errors && (
                   <Alert>
                     {errors.map((el, index) => (
@@ -280,48 +236,88 @@ export default function SignUpPage() {
                     ))}
                   </Alert>
                 )}
+
+                <div id="clerk-captcha"></div>
+
                 <SubmitButton
+                  isPending={isLoading}
                   className="w-full"
                   type="submit"
-                  text="Verify Email"
-                  loadingText="Verifying Email"
+                  text="Sign Up"
+                  loadingText="Signing you up"
                 />
+                <Separator text="Or" />
+                <OauthSignIn />
               </form>
-            )}
-          </CardContent>
-          <CardFooter className="justify-center flex flex-col items-center text-sm">
-            {!pendingVerification && (
-              <>
-                <p className="text-muted-foreground">
-                  Already have an account?{" "}
-                  <Link
-                    href="/login"
-                    className="font-medium text-primary hover:underline"
-                  >
-                    Sign In
-                  </Link>
-                </p>{" "}
-                <p className="text-sm text-muted-foreground mt-4">
-                  By signing up to create an account, you are accepting our{" "}
-                  <Link
-                    href="/terms-and-conditions"
-                    className="font-medium text-primary hover:underline"
-                  >
-                    terms
-                  </Link>{" "}
-                  and{" "}
-                  <Link
-                    href="/privacy-policy"
-                    className="font-medium text-primary hover:underline"
-                  >
-                    privacy policy.
-                  </Link>
-                </p>
-              </>
-            )}
-          </CardFooter>
-        </Card>
-      </div>
+            </>
+          ) : (
+            <form onSubmit={handleVerify} className="space-y-4">
+              <div className="space-y-2 flex flex-col items-center justify-center">
+                <Label htmlFor="code">Verification Code</Label>
+                <InputOTP
+                  maxLength={codeLength}
+                  pattern={REGEXP_ONLY_DIGITS_AND_CHARS}
+                  value={code}
+                  onChange={(value) => setCode(value)}
+                  disabled={isLoading}
+                >
+                  <InputOTPGroup>
+                    {[...Array(codeLength)].map((_, i) => (
+                      <InputOTPSlot key={i} index={i} />
+                    ))}
+                  </InputOTPGroup>
+                </InputOTP>
+              </div>
+              {errors && (
+                <Alert>
+                  {errors.map((el, index) => (
+                    <AlertDescription key={index}>
+                      {el.longMessage}
+                    </AlertDescription>
+                  ))}
+                </Alert>
+              )}
+              <SubmitButton
+                className="w-full"
+                type="submit"
+                text="Verify Email"
+                loadingText="Verifying Email"
+              />
+            </form>
+          )}
+        </CardContent>
+        <CardFooter className="justify-center flex flex-col items-center text-sm">
+          {!pendingVerification && (
+            <>
+              <p className="text-muted-foreground">
+                Already have an account?{" "}
+                <Link
+                  href="/login"
+                  className="font-medium text-primary hover:underline"
+                >
+                  Sign In
+                </Link>
+              </p>{" "}
+              <p className="text-sm text-muted-foreground mt-4">
+                By signing up to create an account, you are accepting our{" "}
+                <Link
+                  href="/terms-and-conditions"
+                  className="font-medium text-primary hover:underline"
+                >
+                  terms
+                </Link>{" "}
+                and{" "}
+                <Link
+                  href="/privacy-policy"
+                  className="font-medium text-primary hover:underline"
+                >
+                  privacy policy.
+                </Link>
+              </p>
+            </>
+          )}
+        </CardFooter>
+      </Card>
     </AuthLayout>
   );
 }
